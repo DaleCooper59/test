7885f85b0c66c829db3675297d3570dedd8ec34f
DaleCooper59 <duvinagechristopher1@gmail.com>
2021-03-12 14:19:38 +0100

    update

--- index.php
+++ index.php
@@ -1,15 +1,40 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-   
-   <h1>sfbEBZ</h1>
+<?php  
+            $servername = 'localhost';
+            $username = 'root';
+            $password = '';
+            $dbname = 'pdodb';
+            
+            try{
+                $dbco = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                
+                $sql = "CREATE DATABASE pdodb";
+                
+                $sql = "CREATE TABLE Clients(
+                    Id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                    Nom VARCHAR(30) NOT NULL,
+                    Prenom VARCHAR(30) NOT NULL,
+                    Adresse VARCHAR(70) NOT NULL,
+                    Ville VARCHAR(30) NOT NULL,
+                    Codepostal INT UNSIGNED NOT NULL,
+                    Pays VARCHAR(30) NOT NULL,
+                    Mail VARCHAR(50) NOT NULL,
+                    DateInscription TIMESTAMP,
+                    UNIQUE(Mail))";
 
-   <form action="/index.php" method="post">
-    <input type="name">
-   </form>
-</body>
-</html>
+                    $sql = "INSERT INTO Clients(Nom,Prenom,Adresse,Ville,Codepostal,Pays,Mail)
+                    VALUES('Giraud','Pierre','Quai d\'Europe','Toulon',83000,'France','pierre.giraud@edhec.com')";
+               
+               $dbco->exec($sql);
+                
+                echo 'valeur !';
+            }
+            
+            catch(PDOException $e){
+              echo "Erreur : " . $e->getMessage();
+            }
+
+
+?>
+  
+
